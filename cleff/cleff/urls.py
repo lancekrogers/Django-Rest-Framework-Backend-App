3cc7f6d7bafef9c2821a3a78955e8d959f13d77d
Lance Rogers <lancekincaid1994@gmail.com>
2015-09-15 01:15:35 -0400

    about to add temporary landing page

--- cleff/cleff/urls.py
+++ cleff/cleff/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^profiles/', include('profiles.urls', namespace='profiles'))
+    url(r'^profiles/', include('profiles.urls', namespace='profiles')),
+    url(r'^', include('preapplanding.urls', namespace='preapplanding')),
 ]
