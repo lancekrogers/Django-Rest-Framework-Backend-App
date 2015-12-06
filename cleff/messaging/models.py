f15e547d1783e87955bd8aa50120059d5b022e41
Lance Rogers <lancekincaid1994@gmail.com>
2015-12-06 05:52:32 -0500

    added messaging app from music network, have not commited yet though

---
+++ cleff/messaging/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.conf import settings
+from cleff.profiles.models import Musician
+
+# Create your models here.
+
+
+class MusicianMusicianConversation(models.Model):
+    initializer = models.ForeignKey(Musician, related_name='initializer')
+    musician_one = models.ForeignKey(Musician, related_name='musician_one')
+    musician_two = models.ForeignKey(Musician, related_name='musician_two')
+    messages = models.ManyToManyField('MusMusMessage', blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return 'conversation between {} and {}'.format(self.musician_one, self.musician_two)
+
+
+class MusMusMessage(models.Model):
+    message = models.TextField()
+    conversation = models.ManyToManyField(MusicianMusicianConversation)
+    sender = models.ForeignKey(Musician, related_name='sender')
+    receiver = models.ForeignKey(Musician, related_name='receiver')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.conversation, self.timestamp)
+
+    class Meta:
+        ordering = ['-timestamp']
---
+++ cleff/messaging/views.py
@@ -0,0 +1,87 @@
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, DeleteView
+
+from cleff.profiles.models import Musician
+from .models import MusicianMusicianConversation, MusMusMessage
+
+# Create your views here.
+
+
+class MusicianMusicianConversationListView(ListView):
+    model = MusicianMusicianConversation
+
+    def owner_is_musician_one(self):
+        owner = Musician.objects.get(pk=self.request.user.pk)
+        return MusicianMusicianConversation.objects.filter(musician_one=owner)
+
+    def owner_is_musician_two(self):
+        owner = Musician.objects.get(pk=self.request.user.pk)
+        return MusicianMusicianConversation.objects.filter(musician_two=owner)
+
+    def get_queryset(self):
+        profile = Musician.objects.get(user=self.request.user)
+        return MusicianMusicianConversation.objects.filter(Q(musician_one=profile) | Q(musician_two=profile))
+
+
+class MusicianMusicianConversationDetailView(DetailView):
+    model = MusicianMusicianConversation
+    fields = ['messages']
+
+    def list_of_messages(self):
+        return MusicianMusicianConversation.messages
+
+
+def mm_start_conv(request, receiver_pk):
+    me = Musician.objects.get(pk=request.user.pk)
+    print(me)
+    other = Musician.objects.get(pk=receiver_pk)
+    print(other)
+    redirection = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        print('post mm_start_conv')
+        if not MusicianMusicianConversation.objects.filter(Q(musician_one=me, musician_two=other) |
+                                                           Q(musician_one=other, musician_two=me)):
+            obj = MusicianMusicianConversation.objects.create(
+                initializer=me,
+                musician_one=me,
+                musician_two=other,
+            )
+            obj.save()
+            return redirect('message:musician_conv_detail_view', obj.pk)
+        else:
+            obj = MusicianMusicianConversation.objects.get(Q(musician_one=me, musician_two=other) |
+                                                           Q(musician_one=other, musician_two=me))
+            return redirect('message:musician_conv_detail_view', obj.pk)
+    else:
+        print('doesnt work')
+        return HttpResponseRedirect(redirection)
+
+
+def mm_message_create_view(request, conversation_pk, receiver_pk):
+    message_t = request.POST['memo']
+    conv = MusicianMusicianConversation.objects.get(pk=conversation_pk)
+    receiver = Musician.objects.get(pk=receiver_pk)
+    me = request.user.musician
+    if request.method == 'POST':
+        print('mm_message_create POST')
+        mm_message = MusMusMessage.objects.create(
+            sender=me,
+            receiver=receiver,
+            message=message_t
+        )
+        mm_message.save()
+        print('mm_message saved')
+        conv.messages.add(mm_message)
+        conv.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return render(request, 'messaging/musicianmusicianconversation_detail.html',
+                  {'message_form': message_t})
+
+
+def conversation_delete(request, conversation_pk):
+    if request.POST:
+        instance = MusicianMusicianConversation.objects.get(pk=conversation_pk)
+        instance.delete()
+        return redirect('message:musician_conversations')
